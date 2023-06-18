cedecac575e46ecb61a22a8219f8217d48be53e0
50ra4 <50ra4.dev.dev@gmail.com>
2023-06-18 13:42:44 +0900

    add: task schema test case

---
+++ src/schema/task.test.ts
@@ -0,0 +1,17 @@
+describe('task schema', () => {
+  describe('task.title', () => {
+    it.todo('');
+  });
+  describe('task.description', () => {
+    it.todo('');
+  });
+  describe('task.startDate', () => {
+    it.todo('');
+  });
+  describe('task.endDate', () => {
+    it.todo('');
+  });
+  describe('taskStatus.name', () => {
+    it.todo('');
+  });
+});
