cb127ccabb1dcb6e2980ed0bbf41220488e85c00
50ra4 <50ra4@users.noreply.github.com>
2023-06-18 14:04:39 +0000

    deploy: b6190b24e9abd4977eeb6a211f7892bbf02e5525

--- assets/Page3-383089db.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-e84834e7.js";function r(){return e.jsxs("main",{children:[e.jsx("h2",{children:"Page3"}),e.jsx("div",{children:"Page3 component"})]})}export{r as default};
