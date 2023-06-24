43703807fc523959ea1aac696c919cba56e43d61
50ra4 <50ra4.dev.dev@gmail.com>
2023-06-24 23:09:11 +0900

    update: a11y for FormError

--- src/presentation/components/FormError/FormError.tsx
+++ src/presentation/components/FormError/FormError.tsx
@@ -3,9 +3,16 @@ import React from 'react';
 type FormErrorProps = {
   className?: string;
   message: string;
-  // TODO: アクセシビリティに配慮したpropertyを追加する
 };
 
 export function FormError({ className, message }: FormErrorProps) {
-  return <p className={`text-base text-red-600 ${className}`}>{message}</p>;
+  return (
+    <p
+      role="alert"
+      aria-live="assertive"
+      className={`text-base text-red-600 ${className}`}
+    >
+      {message}
+    </p>
+  );
 }
