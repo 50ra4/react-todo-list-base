ce7db546606ea9f6b0a4d20dbe0deb2fa8bbde01
50ra4 <50ra4.dev.dev@gmail.com>
2023-06-25 18:00:55 +0900

    feat: use RHF for ContactForm

--- src/presentation/pages/TaskInputPage/TaskInputPage.tsx
+++ src/presentation/pages/TaskInputPage/TaskInputPage.tsx
@@ -1,4 +1,6 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Button } from '@/components/Button/Button';
 import { TextForm } from '@/components/TextForm/TextForm';
@@ -8,17 +10,30 @@ const PHONE_NUMBER = new RegExp('^[0-9]{10,11}$');
 const ContactSchema = z.object({
   name: z.string().max(20),
   kana: z.string().max(50).regex(KATAKANA, '全角カタカナで入力してください'),
-  mail: z.string().email(),
+  mail: z.string().email('メール形式で入力してください'),
   tel: z
     .string()
     .regex(PHONE_NUMBER, '半角数字で10、11桁以内で入力してください'),
 });
-
-const preventDefault = (e: React.FormEvent<HTMLElement>) => {
-  e.preventDefault();
+type Contact = z.infer<typeof ContactSchema>;
+const ContactDefaultValue: Contact = {
+  name: '',
+  kana: '',
+  mail: '',
+  tel: '',
 };
 
 function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, ...other },
+    watch,
+  } = useForm<Contact>({
+    resolver: zodResolver(ContactSchema),
+    defaultValues: ContactDefaultValue,
+  });
+
   return (
     <div className="bg-white m-2 p-3 rounded sm:w-96">
       <h2 id="formTitle" className="text-2xl font-bold pb-4">
@@ -27,12 +42,51 @@ function ContactForm() {
       <form
         aria-labelledby="formTitle"
         className="[&>*]:mb-2"
-        onSubmit={preventDefault}
+        onSubmit={handleSubmit((d) => console.log(d))}
       >
-        <TextForm id="name" name="name" label="名前" />
-        <TextForm id="katakana-name" name="katakana name" label="名前(カナ)" />
-        <TextForm id="mail" type="email" name="mail" label="メールアドレス" />
-        <TextForm id="tel" type="tel" name="tel" label="電話番号" />
+        <TextForm
+          ref={register('name').ref}
+          name={register('name').name}
+          errorMessage={errors.name?.message}
+          id="name"
+          label="名前"
+        />
+        <TextForm
+          ref={register('kana').ref}
+          name={register('kana').name}
+          id="katakana-name"
+          label="名前(カナ)"
+          errorMessage={errors.kana?.message}
+        />
+        <TextForm
+          ref={register('mail').ref}
+          name={register('mail').name}
+          id="mail"
+          type="email"
+          label="メールアドレス"
+          errorMessage={errors.mail?.message}
+        />
+        <TextForm
+          ref={register('tel').ref}
+          name={register('tel').name}
+          id="tel"
+          type="tel"
+          label="電話番号"
+          errorMessage={errors.tel?.message}
+        />
+        <div className="flex justify-center mt-4">
+          <Button
+            color="primary"
+            onClick={() => {
+              console.log(register('kana'));
+              console.log(other);
+              console.log(errors);
+              console.log(watch());
+            }}
+          >
+            確認する
+          </Button>
+        </div>
         <div className="flex justify-center mt-4">
           <Button type="submit" color="secondary">
             送信する
