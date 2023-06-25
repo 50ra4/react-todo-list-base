7a1c9bac43a83fd63f889398c3ceb29baaeff37a
50ra4 <50ra4.dev.dev@gmail.com>
2023-06-25 18:31:40 +0900

    chore: ContactPage jsx structure

--- src/presentation/pages/ContactPage/ContactPage.tsx
+++ src/presentation/pages/ContactPage/ContactPage.tsx
@@ -9,7 +9,12 @@ import {
   CONTACT_DEFAULT_VALUE,
 } from 'src/schema/contact';
 
-function ContactForm() {
+const onValid = (contact: Contact) => {
+  console.log('contact', contact);
+  alert('送信しました！');
+};
+
+function ContactPage() {
   const {
     register,
     handleSubmit,
@@ -20,56 +25,50 @@ function ContactForm() {
   });
 
   return (
-    <div className="bg-white m-2 p-3 rounded sm:w-96">
-      <h2 id="formTitle" className="text-2xl font-bold pb-4">
-        連絡先入力
-      </h2>
-      <form
-        aria-labelledby="formTitle"
-        className="[&>*]:mb-2"
-        onSubmit={handleSubmit((d) => console.log(d))}
-      >
-        <TextForm
-          {...register('name')}
-          errorMessage={errors.name?.message}
-          id="name"
-          label="名前"
-        />
-        <TextForm
-          {...register('kana')}
-          id="kana"
-          label="名前(カナ)"
-          errorMessage={errors.kana?.message}
-        />
-        <TextForm
-          {...register('mail')}
-          id="mail"
-          type="email"
-          label="メールアドレス"
-          errorMessage={errors.mail?.message}
-        />
-        <TextForm
-          {...register('tel')}
-          id="tel"
-          type="tel"
-          label="電話番号"
-          errorMessage={errors.tel?.message}
-        />
-        <div className="flex justify-center mt-4">
-          <Button type="submit" color="secondary">
-            送信する
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
-}
-
-function TaskInputPage() {
-  return (
     <main className="flex justify-center justify-items-center">
-      <ContactForm />
+      <div className="bg-white m-2 p-3 rounded sm:w-96">
+        <h2 id="formTitle" className="text-2xl font-bold pb-4">
+          連絡先入力
+        </h2>
+        <form
+          aria-labelledby="formTitle"
+          className="[&>*]:mb-2"
+          onSubmit={handleSubmit(onValid)}
+        >
+          <TextForm
+            {...register('name')}
+            errorMessage={errors.name?.message}
+            id="name"
+            label="名前"
+          />
+          <TextForm
+            {...register('kana')}
+            id="kana"
+            label="名前(カナ)"
+            errorMessage={errors.kana?.message}
+          />
+          <TextForm
+            {...register('mail')}
+            id="mail"
+            type="email"
+            label="メールアドレス"
+            errorMessage={errors.mail?.message}
+          />
+          <TextForm
+            {...register('tel')}
+            id="tel"
+            type="tel"
+            label="電話番号"
+            errorMessage={errors.tel?.message}
+          />
+          <div className="flex justify-center mt-4">
+            <Button type="submit" color="secondary">
+              送信する
+            </Button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
-export default TaskInputPage;
+export default ContactPage;
