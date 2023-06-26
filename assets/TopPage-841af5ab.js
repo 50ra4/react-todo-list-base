ded01050243dedceeed41608b55184edc2fe0191
50ra4 <50ra4@users.noreply.github.com>
2023-06-26 12:53:35 +0000

    deploy: b57d34eba931b4897a15e6fd5086598e07203ddb

--- assets/TopPage-841af5ab.js
+++
@@ -1,1 +0,0 @@
-import{j as n,L as e}from"./index-2db75537.js";function t(){return n.jsxs("main",{children:[n.jsx("h2",{children:"TopPage"}),n.jsx("div",{children:"TopPage component"}),n.jsx("ul",{children:n.jsx("li",{children:n.jsx(e,{to:"/contact",children:"Go to Contact Page!"})})})]})}export{t as default};
--- storybook/assets/preview-00954f6c.js
+++ storybook/assets/preview-00954f6c.js
@@ -1,2 +1,2 @@
 const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{o as parameters};
-//# sourceMappingURL=preview-6ed480a3.js.map
+//# sourceMappingURL=preview-00954f6c.js.map
