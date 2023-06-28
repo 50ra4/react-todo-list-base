fa3b4fa24ea95268bbd0cf5191f5c2d538116104
50ra4 <50ra4.dev.dev@gmail.com>
2023-06-28 22:40:00 +0900

    add: ArrowDropDownIcon

---
+++ src/presentation/components/icons/ArrowDropDownIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { SvgIcon, IconProps } from './SvgIcon';
+
+/**
+ * @see https://fonts.google.com/icons
+ */
+export function ArrowDropDownIcon(props: IconProps) {
+  return (
+    <SvgIcon {...props} viewBox="0 -960 960 960">
+      <path d="M480-328 225-583h510L480-328Z" />
+    </SvgIcon>
+  );
+}
