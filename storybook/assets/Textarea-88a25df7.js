c0c161a34b25ac5c34fc52488d0326c27c81ef30
50ra4 <50ra4@users.noreply.github.com>
2023-07-05 15:15:29 +0000

    deploy: 83d51c4916856eb36209d2688fcf7ac7e8a7a7b2

---
+++ storybook/assets/Textarea-88a25df7.js
@@ -0,0 +1,7 @@
+import{j as p}from"./jsx-runtime-6eef64cc.js";import{R as c}from"./index-c013ead5.js";const e=c.forwardRef(function({className:r,id:n,name:l,placeholder:t,isInvalid:d,value:i,describedId:s,onChange:u,onBlur:o},m){return p.jsx("textarea",{ref:m,className:`border border-gray-300 border-solid rounded-sm
+        w-full h-24 resize-y
+        text-lg
+        py-0.5 px-1
+        ${d?"border-red-600 bg-red-200":""}
+        ${r||""}`,id:n,name:l,placeholder:t,value:i,"aria-describedby":s,onChange:u,onBlur:o})});try{e.displayName="Textarea",e.__docgenInfo={description:"",displayName:"Textarea",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},id:{defaultValue:null,description:"",name:"id",required:!0,type:{name:"string"}},name:{defaultValue:null,description:"",name:"name",required:!0,type:{name:"string"}},value:{defaultValue:null,description:"",name:"value",required:!1,type:{name:"string"}},placeholder:{defaultValue:null,description:"",name:"placeholder",required:!1,type:{name:"string"}},isInvalid:{defaultValue:null,description:"",name:"isInvalid",required:!1,type:{name:"boolean"}},describedId:{defaultValue:null,description:"",name:"describedId",required:!1,type:{name:"string"}},onChange:{defaultValue:null,description:"",name:"onChange",required:!1,type:{name:"ChangeEventHandler<HTMLTextAreaElement>"}},onBlur:{defaultValue:null,description:"",name:"onBlur",required:!1,type:{name:"ChangeEventHandler<HTMLTextAreaElement>"}}}}}catch{}export{e as T};
+//# sourceMappingURL=Textarea-88a25df7.js.map
---
+++ storybook/assets/Textarea.stories-e6c0f3e8.js
@@ -0,0 +1,7 @@
+import{j as i}from"./jsx-runtime-6eef64cc.js";import{T as o}from"./Textarea-88a25df7.js";import{d as a}from"./forLinter-9fb37d9d.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";const s={id:"textarea",name:"comments",value:`いつも楽しみにしています。
+これからも頑張ってください！！`,placeholder:"自由に入力してください",onChange:a,onBlur:a},S={component:o,args:{...s}},r={render:()=>i.jsx(o,{...s})},e={render:()=>i.jsx(o,{...s,isInvalid:!0})};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <Component {...PROPS} />
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var c,p,d;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => <Component {...PROPS} isInvalid={true} />
+}`,...(d=(p=e.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const f=["Docs","Error"];export{r as Docs,e as Error,f as __namedExportsOrder,S as default};
+//# sourceMappingURL=Textarea.stories-e6c0f3e8.js.map
