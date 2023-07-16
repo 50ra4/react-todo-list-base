c2bb59f92cf1ff24a4227cddad2b60ba07095647
50ra4 <50ra4@users.noreply.github.com>
2023-07-16 07:04:35 +0000

    deploy: dd2b568fbc8c99b532574d1cf7c7dd695fd36c5b

---
+++ assets/ErrorPage-0f79d80a.js
@@ -0,0 +1,1 @@
+import{j as r,L as e}from"./index-dedc29e2.js";function o(){return r.jsxs("main",{children:[r.jsx("h2",{children:"Error Page"}),r.jsx(e,{to:"/",children:"TopPage"})]})}export{o as default};
