2cb8597a1bc6c46fe912d24771bb88a4b7da73cb
50ra4 <50ra4@users.noreply.github.com>
2023-07-16 06:19:33 +0000

    deploy: 11313eacd83b4cfdb17081d3d2385cf037792239

---
+++ assets/ErrorPage-90082c3a.js
@@ -0,0 +1,1 @@
+import{j as r,L as e}from"./index-ec2804f2.js";function o(){return r.jsxs("main",{children:[r.jsx("h2",{children:"Error Page"}),r.jsx(e,{to:"/",children:"TopPage"})]})}export{o as default};
--- assets/ErrorPage-c0a02622.js
+++
@@ -1,1 +0,0 @@
-import{j as r,L as e}from"./index-79528676.js";function o(){return r.jsxs("main",{children:[r.jsx("h2",{children:"Error Page"}),r.jsx(e,{to:"/",children:"TopPage"})]})}export{o as default};
